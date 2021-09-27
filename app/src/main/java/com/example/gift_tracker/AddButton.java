57860456d0bee5e5493c977e321a6cf447e50e4e
Kyle <kwchristensen@gmail.com>
2021-09-26 23:28:39 -0700

    recipient and gift tabs identified in AddButton, switch not working idk why

--- app/src/main/java/com/example/gift_tracker/AddButton.java
+++ app/src/main/java/com/example/gift_tracker/AddButton.java
@@ -1,14 +1,38 @@
 package com.example.gift_tracker;
 
+import android.util.Log;
 import android.view.View;
 
 import com.google.android.material.snackbar.Snackbar;
 
 public class AddButton {
 
+    private static String LOG = "buttonTag";
+    String message;
+
     public AddButton(View view, int tabIndex) {
-            Snackbar.make(view, String.valueOf(tabIndex), Snackbar.LENGTH_LONG)
-            .setAction("Action", null).show();
+
+        Log.d(LOG, String.valueOf(tabIndex));
+
+/*        switch(tabIndex) {
+            case 0:
+                message = "Recipient Tab";
+                Log.d(LOG, "zero");
+            case 1:
+                message = "Gift Tab";
+                Log.d(LOG, "one");
+            default:
+                message = "error";
+        }*/
+
+        if(tabIndex == 0) {
+            Log.d(LOG,"Recipient");
+        } else if (tabIndex == 1) {
+            Log.d(LOG, "Gift");
+        }
+
+        Snackbar.make(view, message, Snackbar.LENGTH_LONG)
+        .setAction("Action", null).show();
 
 
 
