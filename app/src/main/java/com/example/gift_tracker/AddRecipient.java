dfa5206fd7c15e046b908ae124433d6f315cdf0e
Kyle <kwchristensen@gmail.com>
2021-09-28 09:10:24 -0700

    adding recipient entry functionality

--- app/src/main/java/com/example/gift_tracker/AddRecipient.java
+++ app/src/main/java/com/example/gift_tracker/AddRecipient.java
@@ -3,12 +3,37 @@ package com.example.gift_tracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddRecipient extends AppCompatActivity {
+
+    private Button btnSubmit;
+    private EditText txtRecipientName;
+    private Recipient newRecipient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_recipient);
+
+        txtRecipientName = findViewById(R.id.textInputRecipientName);
+
+        btnSubmit = findViewById(R.id.btnRecipientSubmit);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!txtRecipientName.getText().toString().equals("")) {
+                //if(txtRecipientName.getText().toString() == null) {
+                    Toast toast = new Toast(getApplicationContext());
+                    toast.setText("Name Required");
+                    toast.show();
+                }
+            }
+        });
     }
+
+
 }
